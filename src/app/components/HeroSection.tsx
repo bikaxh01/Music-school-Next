944078f886241ffc291c1a5dd0168b41c54c53b5
Bikash Mishra <mishrabikash202@gmail.com>
2024-04-02 04:44:36 +0530

    first commit

---
+++ src/app/components/HeroSection.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import React from "react";
+
+function HeroSection() {
+  return (
+    <div className=" h-auto md:h-[40rem] w-full rounded-md flex flex-col overflow-hidden mx-auto items-center relative justify-center py-10 md:py-0">
+      <div className=" p-4 relative z-10 w-full text-center">
+        <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">
+          Master the art of music
+        </h1>
+        <p className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto">
+          Dive into our comprehensive music courses and transform your musical
+          journey today. Whether you're a beginner or looking to refine your
+          skills, join us to unlock your true potential.
+        </p>
+        <div className=" mt-4">
+          <Link  href= "/courses" className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+            <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+            <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+             Explore Courses
+            </span>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default HeroSection;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 import Image from "next/image";
+import HeroSection from "./components/HeroSection";
 
 export default function Home() {
   return (
-   <h1>HEl</h1>
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <HeroSection/>
+    </main>
   );
 }
